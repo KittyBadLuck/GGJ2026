23a125f22f33e28982548876ff25dec941cd2067
Lou de Tarade <117767124+KittyBadLuck@users.noreply.github.com>
2026-01-31 16:50:09 -0500

    Add ui buttons

--- Assets/Scripts/UI/LevelSelectorMenu.cs
+++ Assets/Scripts/UI/LevelSelectorMenu.cs
@@ -12,11 +12,13 @@ public class LevelSelectorMenu : MonoBehaviour
     private void Start()
     {
         bool precedentIsUnlocked = true;
+        int index = 0;
         foreach (var s in puzzleSceneList)
         {
             var go = Instantiate(levelSelectorPrefab, gridParent);
             LevelSelector lS = go.GetComponent<LevelSelector>();
-            lS.SetUp(precedentIsUnlocked, s);
+            lS.SetUp(precedentIsUnlocked, s, index);
+            index++;
 
             float score = PlayerPrefs.GetFloat(s, 0);
             precedentIsUnlocked =( score / 100) >( 1 - PuzzleManager.WIN_THRESHOLD);
